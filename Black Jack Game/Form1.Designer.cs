deb424983597f5a3d6aa90a2d581a495179b6c51
Jordan Couperthwaite@ <jbcouperthwaite@gmail.com>
2018-06-15 02:14:51 +0200

    Game Reset method
    Working on a method to reset the game back to the start as well as added code to fill a Sorted Array with pathfiles to the image of each othe cards.

--- Black Jack Game/Form1.Designer.cs
+++ Black Jack Game/Form1.Designer.cs
@@ -33,29 +33,47 @@
             this.btnHit = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnHelp = new System.Windows.Forms.Button();
-            this.pBoxPlayer1 = new System.Windows.Forms.PictureBox();
             this.pBoxPlayer9 = new System.Windows.Forms.PictureBox();
             this.pBoxPlayer8 = new System.Windows.Forms.PictureBox();
             this.pBoxPlayer7 = new System.Windows.Forms.PictureBox();
+            this.pBoxPlayer6 = new System.Windows.Forms.PictureBox();
             this.pBoxPlayer5 = new System.Windows.Forms.PictureBox();
             this.pBoxPlayer4 = new System.Windows.Forms.PictureBox();
-            this.pBoxPlayer6 = new System.Windows.Forms.PictureBox();
             this.pBoxPlayer3 = new System.Windows.Forms.PictureBox();
             this.pBoxPlayer2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer1)).BeginInit();
+            this.pBoxPlayer1 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer1 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer9 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer8 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer7 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer6 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer5 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer4 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer3 = new System.Windows.Forms.PictureBox();
+            this.pBoxDealer2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer9)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnNewGame
             // 
-            this.btnNewGame.Location = new System.Drawing.Point(910, 409);
+            this.btnNewGame.Location = new System.Drawing.Point(910, 391);
             this.btnNewGame.Name = "btnNewGame";
             this.btnNewGame.Size = new System.Drawing.Size(75, 23);
             this.btnNewGame.TabIndex = 0;
@@ -64,6 +82,7 @@
             // 
             // btnStand
             // 
+            this.btnStand.Enabled = false;
             this.btnStand.Location = new System.Drawing.Point(671, 467);
             this.btnStand.Name = "btnStand";
             this.btnStand.Size = new System.Drawing.Size(75, 23);
@@ -73,7 +92,8 @@
             // 
             // btnHit
             // 
-            this.btnHit.Location = new System.Drawing.Point(671, 380);
+            this.btnHit.Enabled = false;
+            this.btnHit.Location = new System.Drawing.Point(671, 391);
             this.btnHit.Name = "btnHit";
             this.btnHit.Size = new System.Drawing.Size(75, 23);
             this.btnHit.TabIndex = 2;
@@ -99,21 +119,12 @@
             this.btnHelp.Text = "Help";
             this.btnHelp.UseVisualStyleBackColor = true;
             // 
-            // pBoxPlayer1
-            // 
-            this.pBoxPlayer1.InitialImage = null;
-            this.pBoxPlayer1.Location = new System.Drawing.Point(100, 362);
-            this.pBoxPlayer1.Name = "pBoxPlayer1";
-            this.pBoxPlayer1.Size = new System.Drawing.Size(99, 128);
-            this.pBoxPlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pBoxPlayer1.TabIndex = 5;
-            this.pBoxPlayer1.TabStop = false;
-            // 
             // pBoxPlayer9
             // 
             this.pBoxPlayer9.Location = new System.Drawing.Point(388, 362);
             this.pBoxPlayer9.Name = "pBoxPlayer9";
             this.pBoxPlayer9.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pBoxPlayer9.TabIndex = 6;
             this.pBoxPlayer9.TabStop = false;
             this.pBoxPlayer9.Visible = false;
@@ -123,6 +134,7 @@
             this.pBoxPlayer8.Location = new System.Drawing.Point(352, 362);
             this.pBoxPlayer8.Name = "pBoxPlayer8";
             this.pBoxPlayer8.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pBoxPlayer8.TabIndex = 7;
             this.pBoxPlayer8.TabStop = false;
             this.pBoxPlayer8.Visible = false;
@@ -132,15 +144,27 @@
             this.pBoxPlayer7.Location = new System.Drawing.Point(316, 362);
             this.pBoxPlayer7.Name = "pBoxPlayer7";
             this.pBoxPlayer7.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pBoxPlayer7.TabIndex = 8;
             this.pBoxPlayer7.TabStop = false;
             this.pBoxPlayer7.Visible = false;
+            // 
+            // pBoxPlayer6
+            // 
+            this.pBoxPlayer6.Location = new System.Drawing.Point(280, 362);
+            this.pBoxPlayer6.Name = "pBoxPlayer6";
+            this.pBoxPlayer6.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxPlayer6.TabIndex = 11;
+            this.pBoxPlayer6.TabStop = false;
+            this.pBoxPlayer6.Visible = false;
             // 
             // pBoxPlayer5
             // 
             this.pBoxPlayer5.Location = new System.Drawing.Point(244, 362);
             this.pBoxPlayer5.Name = "pBoxPlayer5";
             this.pBoxPlayer5.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pBoxPlayer5.TabIndex = 9;
             this.pBoxPlayer5.TabStop = false;
             this.pBoxPlayer5.Visible = false;
@@ -150,18 +174,10 @@
             this.pBoxPlayer4.Location = new System.Drawing.Point(208, 362);
             this.pBoxPlayer4.Name = "pBoxPlayer4";
             this.pBoxPlayer4.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pBoxPlayer4.TabIndex = 10;
             this.pBoxPlayer4.TabStop = false;
             this.pBoxPlayer4.Visible = false;
-            // 
-            // pBoxPlayer6
-            // 
-            this.pBoxPlayer6.Location = new System.Drawing.Point(280, 362);
-            this.pBoxPlayer6.Name = "pBoxPlayer6";
-            this.pBoxPlayer6.Size = new System.Drawing.Size(99, 128);
-            this.pBoxPlayer6.TabIndex = 11;
-            this.pBoxPlayer6.TabStop = false;
-            this.pBoxPlayer6.Visible = false;
             // 
             // pBoxPlayer3
             // 
@@ -169,6 +185,7 @@
             this.pBoxPlayer3.Location = new System.Drawing.Point(172, 362);
             this.pBoxPlayer3.Name = "pBoxPlayer3";
             this.pBoxPlayer3.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pBoxPlayer3.TabIndex = 12;
             this.pBoxPlayer3.TabStop = false;
             this.pBoxPlayer3.Visible = false;
@@ -179,15 +196,131 @@
             this.pBoxPlayer2.Location = new System.Drawing.Point(136, 362);
             this.pBoxPlayer2.Name = "pBoxPlayer2";
             this.pBoxPlayer2.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pBoxPlayer2.TabIndex = 13;
             this.pBoxPlayer2.TabStop = false;
             this.pBoxPlayer2.Visible = false;
+            // 
+            // pBoxPlayer1
+            // 
+            this.pBoxPlayer1.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxPlayer1.Location = new System.Drawing.Point(100, 362);
+            this.pBoxPlayer1.Name = "pBoxPlayer1";
+            this.pBoxPlayer1.Size = new System.Drawing.Size(99, 128);
+            this.pBoxPlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxPlayer1.TabIndex = 5;
+            this.pBoxPlayer1.TabStop = false;
+            // 
+            // pBoxDealer1
+            // 
+            this.pBoxDealer1.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer1.Location = new System.Drawing.Point(100, 69);
+            this.pBoxDealer1.Name = "pBoxDealer1";
+            this.pBoxDealer1.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer1.TabIndex = 14;
+            this.pBoxDealer1.TabStop = false;
+            // 
+            // pBoxDealer9
+            // 
+            this.pBoxDealer9.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer9.Location = new System.Drawing.Point(388, 69);
+            this.pBoxDealer9.Name = "pBoxDealer9";
+            this.pBoxDealer9.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer9.TabIndex = 15;
+            this.pBoxDealer9.TabStop = false;
+            this.pBoxDealer9.Visible = false;
+            // 
+            // pBoxDealer8
+            // 
+            this.pBoxDealer8.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer8.Location = new System.Drawing.Point(352, 69);
+            this.pBoxDealer8.Name = "pBoxDealer8";
+            this.pBoxDealer8.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer8.TabIndex = 16;
+            this.pBoxDealer8.TabStop = false;
+            this.pBoxDealer8.Visible = false;
+            // 
+            // pBoxDealer7
+            // 
+            this.pBoxDealer7.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer7.Location = new System.Drawing.Point(316, 69);
+            this.pBoxDealer7.Name = "pBoxDealer7";
+            this.pBoxDealer7.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer7.TabIndex = 17;
+            this.pBoxDealer7.TabStop = false;
+            this.pBoxDealer7.Visible = false;
+            // 
+            // pBoxDealer6
+            // 
+            this.pBoxDealer6.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer6.Location = new System.Drawing.Point(280, 69);
+            this.pBoxDealer6.Name = "pBoxDealer6";
+            this.pBoxDealer6.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer6.TabIndex = 18;
+            this.pBoxDealer6.TabStop = false;
+            this.pBoxDealer6.Visible = false;
+            // 
+            // pBoxDealer5
+            // 
+            this.pBoxDealer5.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer5.Location = new System.Drawing.Point(244, 69);
+            this.pBoxDealer5.Name = "pBoxDealer5";
+            this.pBoxDealer5.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer5.TabIndex = 19;
+            this.pBoxDealer5.TabStop = false;
+            this.pBoxDealer5.Visible = false;
+            // 
+            // pBoxDealer4
+            // 
+            this.pBoxDealer4.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer4.Location = new System.Drawing.Point(208, 69);
+            this.pBoxDealer4.Name = "pBoxDealer4";
+            this.pBoxDealer4.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer4.TabIndex = 20;
+            this.pBoxDealer4.TabStop = false;
+            this.pBoxDealer4.Visible = false;
+            // 
+            // pBoxDealer3
+            // 
+            this.pBoxDealer3.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer3.Location = new System.Drawing.Point(172, 69);
+            this.pBoxDealer3.Name = "pBoxDealer3";
+            this.pBoxDealer3.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer3.TabIndex = 21;
+            this.pBoxDealer3.TabStop = false;
+            this.pBoxDealer3.Visible = false;
+            // 
+            // pBoxDealer2
+            // 
+            this.pBoxDealer2.InitialImage = global::Black_Jack_Game.Properties.Resources.Card_Back;
+            this.pBoxDealer2.Location = new System.Drawing.Point(136, 69);
+            this.pBoxDealer2.Name = "pBoxDealer2";
+            this.pBoxDealer2.Size = new System.Drawing.Size(99, 128);
+            this.pBoxDealer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxDealer2.TabIndex = 22;
+            this.pBoxDealer2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1038, 536);
+            this.Controls.Add(this.pBoxDealer9);
+            this.Controls.Add(this.pBoxDealer8);
+            this.Controls.Add(this.pBoxDealer7);
+            this.Controls.Add(this.pBoxDealer6);
+            this.Controls.Add(this.pBoxDealer5);
+            this.Controls.Add(this.pBoxDealer4);
+            this.Controls.Add(this.pBoxDealer3);
+            this.Controls.Add(this.pBoxDealer2);
             this.Controls.Add(this.pBoxPlayer9);
             this.Controls.Add(this.pBoxPlayer8);
             this.Controls.Add(this.pBoxPlayer7);
@@ -202,17 +335,27 @@
             this.Controls.Add(this.btnStand);
             this.Controls.Add(this.btnNewGame);
             this.Controls.Add(this.pBoxPlayer1);
+            this.Controls.Add(this.pBoxDealer1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer9)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDealer2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -233,6 +376,15 @@
         private System.Windows.Forms.PictureBox pBoxPlayer6;
         private System.Windows.Forms.PictureBox pBoxPlayer3;
         private System.Windows.Forms.PictureBox pBoxPlayer2;
+        private System.Windows.Forms.PictureBox pBoxDealer1;
+        private System.Windows.Forms.PictureBox pBoxDealer9;
+        private System.Windows.Forms.PictureBox pBoxDealer8;
+        private System.Windows.Forms.PictureBox pBoxDealer7;
+        private System.Windows.Forms.PictureBox pBoxDealer6;
+        private System.Windows.Forms.PictureBox pBoxDealer5;
+        private System.Windows.Forms.PictureBox pBoxDealer4;
+        private System.Windows.Forms.PictureBox pBoxDealer3;
+        private System.Windows.Forms.PictureBox pBoxDealer2;
     }
 }
 
--- Black Jack Game/Properties/Resources.Designer.cs
+++ Black Jack Game/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace Black_Jack_Game.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Card_Back {
+            get {
+                object obj = ResourceManager.GetObject("Card Back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap CJ {
             get {
                 object obj = ResourceManager.GetObject("CJ", resourceCulture);
